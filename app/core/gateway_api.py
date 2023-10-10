7844d45dfcc27ddea9ae227200b01e679e96852e
Jafar Esmaili <jaffar9898@gmail.com>
2023-10-11 02:00:02 +0330

    fix error gateway api

--- app/core/gateway_api.py
+++ app/core/gateway_api.py
@@ -4,57 +4,59 @@ import random
 from app.core.db import GatewayModel
 from app.core.gateways.kavenegar import KavenegarGateway
 from app.core.gateways.ghasedak import GhasedakGateway
- 
 
 
 class GatewayApi:
-    def __init__(self, **kwargs):
+    def __init__(self):
         self.gateway = None
         self.tracking_number = str(uuid4())
 
-    def init_gateway(self):
-        gateway = GatewayModel.objects.filter(status=True, types__key=bill_type.rstrip("_bill")).order_by(
-            "-priority").last()
+    async def init_gateway(self, service_name: str):
+        gateway = await GatewayModel.objects.filter(active=True).order_by("priority").first()
         if not gateway:
             return False
         if gateway.title == "kavenegar":
             self.gateway = KavenegarGateway(gateway)
         if gateway.title == "ghasedak":
             self.gateway = GhasedakGateway(gateway)
-      
-        return True
 
-    def send(self, payload: dict):
-        answer = {"success": False, "msg": "خطای رخ داده هست"}
-        
-        if not self.init_gateway():
-            answer["msg"] = "سرویس دهنده پشتیبانی نمی کند"
+        return service_name in dir(self.gateway)
+
+    async def send(self, payload: dict):
+        answer = {"success": False, "message": "خطای رخ داده هست", "data": None}
+        init = await self.init_gateway('send')
+        if not init:
+            answer["message"] = "سرویس دهنده پشتیبانی نمی کند"
             return answer, 400
-        
-        resp = self.gateway.send()
+
+        resp = await self.gateway.send(payload)
+
         json_response = {
-            "data": resp["data"] if resp["success"] else None,
-            "success": resp["success"],
-            "message": resp["msg"],
+            "data": None,
+            "success": True if resp else False,
+            "message": "ok",
         }
-        state_code = 200 if resp["success"] else 400
+        state_code = 200 if resp else 400
 
         return json_response, state_code
 
-    def send_verify(self,   payload: dict = {}):
-        answer = {"success": False, "msg": "خطای رخ داده هست"}
-        if not self.has_support_bill_type(bill_type):
-            answer["msg"] = "سرویس دهنده پشتیبانی نمی کند"
+    async def send_verify(self, payload: dict = {}):
+        answer = {"success": False, "message": "خطای رخ داده هست", "data": None}
+        init = await self.init_gateway('send_verify')
+        if not init:
+            answer["message"] = "سرویس دهنده پشتیبانی نمی کند"
             return answer, 400
 
-        bill_type = f"pay_{bill_type}_bill"
-        resp = getattr(self.provider, bill_type)(payload)
+        resp = await self.gateway.send(payload)
+
         json_response = {
-            "data": resp["data"],
-            "success": resp["success"],
-            "message": resp["msg"],
+            "data": None,
+            "success": True if resp else False,
+            "message": "ok",
         }
-        state_code = 200 if resp["success"] else 400
+        state_code = 200 if resp else 400
 
         return json_response, state_code
- 
+
+
+gateway_api = GatewayApi()
