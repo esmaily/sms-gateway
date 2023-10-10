98717f101a1f6c4d414de8d098f5aadc3800e6a2
Jafar Esmaili <jaffar9898@gmail.com>
2023-10-11 01:59:41 +0330

    implemenet web services

--- app/core/gateways/ghasedak.py
+++ app/core/gateways/ghasedak.py
@@ -1,54 +1,27 @@
+from uuid import uuid4
+from app.core.db import GatewayModel
+import ghasedakpack
 
 
 class GhasedakGateway:
-    def __init__(self,instance: GatewayModel ):
+    def __init__(self, instance: GatewayModel):
         self.model = instance
-        self.headers = {
-            'Accept-Language': "fa",
-            'cache-control': "no-cache",
-        }
+        self.tracking_number = str(uuid4())
+        self.api = ghasedakpack.Ghasedak(self.model.token)
 
-    def send_request(self, path_url, payload):
-        url = FARABOOM['url'] + path_url
-        api_call = APICall(provider_type.FARABOOM_BILL_INQUIRY)
-        try:
-            response = api_call.rest_request(
-                url=url, method='post', json=payload, headers=self.headers, timeout=FARABOOM_TIMEOUT, verify=False)
-            if response.status_code == 200:
-                response = api_call.json_serializer(answer=response)
-                success = True
-                state = GeneralStatusCode.SUCCESS
-            else:
-                json_resp = api_call.json_serializer(answer=response)
-                response = json_resp['errors'][0]
-                state = faraboom_error_codes.get(response.get('code', {}), FaraboomStatusCodes.FARABOOM_UNKNOWN_ERROR)
-                success = False
-            return response, success, state
-        except Exception as e:
-            from gpay import tasks
-            tasks.send_provider_alarm.delay(
-                is_transaction=False,
-                service_name=provider_type.FARABOOM_BILL_INQUIRY,
-                message=str(e),
-                response=str(e),
-                title=BillStatusCodes.BILL_INQUIRY_SERVICE_EXCEPTION.message
-            )
-            return {'exception': str(e)}, False, GeneralStatusCode.PROVIDER_IS_NOT_AVAILABLE
-
-    def send(self, **kwargs):
-        payload = {
-            "bill_id": kwargs['bill_id']
+    async def send(self, payload: dict):
+        params = {
+            'message': payload["text"],
+            'receptor': payload["mobile"],
+            'linenumber': 'xxxx',
+            'checkid': self.tracking_number
         }
-        response = self.send_request(path_url="vas/tavanir/bills", payload=payload)
-        self.recording_request(type='electricity', data=payload, response=response)
+        response = self.api.send(params)
         return response
 
-    def send_verify(self, **kwargs):
-        payload = {
-            "bill_id": kwargs['bill_id']
+    async def send_verify(self, payload: dict):
+        params = {
+            {'receptor': payload["mobile"], 'type': '1', 'template': 'Your', 'param1': '', 'param2': '', 'param3': ''}
         }
-        response = self.send_request(path_url="vas/abfa/bills", payload=payload)
-        self.recording_request(type='abfa', data=payload, response=response)
+        response = self.api.verification(params)
         return response
-
-  
--- app/core/gateways/kavenegar.py
+++ app/core/gateways/kavenegar.py
@@ -1,54 +1,28 @@
-
 from app.core.db import GatewayModel
+from kavenegar import *
+
+
 class KavenegarGateway:
     def __init__(self, instance: GatewayModel):
         self.model = instance
-        self.headers = {
-            'Accept-Language': "fa",
-            'cache-control': "no-cache",
-        }
+        self.tracking_number = str(uuid4())
+        self.api = KavenegarAPI(instance.token, timeout=20)
 
-    def send_request(self, path_url, payload):
-        url = FARABOOM['url'] + path_url
-        api_call = APICall(provider_type.FARABOOM_BILL_INQUIRY)
-        try:
-            response = api_call.rest_request(
-                url=url, method='post', json=payload, headers=self.headers, timeout=FARABOOM_TIMEOUT, verify=False)
-            if response.status_code == 200:
-                response = api_call.json_serializer(answer=response)
-                success = True
-                state = GeneralStatusCode.SUCCESS
-            else:
-                json_resp = api_call.json_serializer(answer=response)
-                response = json_resp['errors'][0]
-                state = faraboom_error_codes.get(response.get('code', {}), FaraboomStatusCodes.FARABOOM_UNKNOWN_ERROR)
-                success = False
-            return response, success, state
-        except Exception as e:
-            from gpay import tasks
-            tasks.send_provider_alarm.delay(
-                is_transaction=False,
-                service_name=provider_type.FARABOOM_BILL_INQUIRY,
-                message=str(e),
-                response=str(e),
-                title=BillStatusCodes.BILL_INQUIRY_SERVICE_EXCEPTION.message
-            )
-            return {'exception': str(e)}, False, GeneralStatusCode.PROVIDER_IS_NOT_AVAILABLE
-
-    def send(self, **kwargs):
-        payload = {
-            "bill_id": kwargs['bill_id']
+    async def send(self, payload: dict):
+        params = {
+            'message': payload["text"],
+            'receptor': payload["mobile"],
+            'sender': 'xxxx',
         }
-        response = self.send_request(path_url="vas/tavanir/bills", payload=payload)
-        self.recording_request(type='electricity', data=payload, response=response)
+        response = self.api.sms_send(params)
         return response
 
-    def send_verify(self, **kwargs):
-        payload = {
-            "bill_id": kwargs['bill_id']
+    async def send_verify(self, payload: dict):
+        params = {
+            'receptor': payload["mobile"],
+            'template': '',
+            'token': '',
+            'type': 'sms',  # sms vs call
         }
-        response = self.send_request(path_url="vas/abfa/bills", payload=payload)
-        self.recording_request(type='abfa', data=payload, response=response)
+        response = self.sms_call.verify_lookup(params)
         return response
-
-  
